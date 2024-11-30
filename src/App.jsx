739768f4855d1b4910c4f8be7410b3aa08aec697
Zettex <safaandsafa4@gmail.com>
2024-11-30 18:47:38 +0600

    primary todo done

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,62 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import Todo from './todo'
-import React from 'react'
-function App() {
-  // const [count, setCount] = useState(0)
-  const [todoArray,setTodoArray] = React.useState(null) 
+import React from 'react';
 
-  
+function App() {
+  const [todoArray, setTodoArray] = React.useState([]);
+
+  function addTask(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const taskDescription = formData.get('todoInfo');
+    const priority = formData.get('color');
+    const completion = formData.get('completion');
+    const FormInfo = {
+      taskDescription,
+      priority,
+      completion,
+    };
+    setTodoArray((prev) => [...prev, FormInfo]);
+  }
+
+  const todoArrayList = todoArray.map((task, index) => (
+    <p key={index}>
+      <strong>Task:</strong> {task.taskDescription} <br />
+      <strong>Priority:</strong> {task.priority} <br />
+      <strong>Completion:</strong> {task.completion}
+    </p>
+  ));
+
   return (
     <>
-      <form >
-        <textarea name="todoInfo" id="todoInfo" defaultValue='Write a Todo here'></textarea>
-        <p>Pririty List:</p>
+      <form onSubmit={addTask}>
+        <textarea name="todoInfo" id="todoInfo" defaultValue="Write a Todo here"></textarea>
+        <p>Priority List:</p>
         <label>
-          <input type="radio" name="color" id="important" value='important' />
+          <input type="radio" name="color" id="important" value="important" />
           Important
         </label>
         <label>
-          <input type="radio" name="color" id="later" value='later'/>
+          <input type="radio" name="color" id="later" value="later" />
           Can be done later
         </label>
         <br />
-        Finshed?<br />
+
+        <p>Finished?</p>
         <label>
-          <input type="checkbox" name="completion" id="finished" value='finished' />
+          <input type="radio" name="completion" id="finished" value="finished" />
           Done
         </label>
         <label>
-          <input type="checkbox" name="completion" id="not_finished" value='not finished' />
+          <input type="radio" name="completion" id="not_finished" value="not finished" />
           Not Done
-        </label><br />
-        <input type="submit" value="Add Task" />
+        </label>
+        <br />
+        <button type="submit">Add Task</button>
       </form>
-      <Todo/>
+
+      <h3>Your Tasks are shown below:</h3>
+      <h5>{todoArrayList}</h5>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/todo.jsx
+++ src/todo.jsx
@@ -1,7 +1,10 @@
-export default function todo(){
+export default function Todo(){
+    
     return(
         <section>
             <h3>Your Tasks for the day:</h3>
+            
+            
         </section>
     )
 }
